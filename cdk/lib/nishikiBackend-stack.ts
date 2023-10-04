f86ca1f0daf796dc10990c102f5cb5687c80d856
hirohata <43802663+hitohata@users.noreply.github.com>
2023-10-04 09:40:33 -0700

    cdk initialization done (#6)
    * cdk init
    
    * fix variable

---
+++ cdk/lib/nishikiBackend-stack.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as Dynamo from "aws-cdk-lib/aws-dynamodb";
+
+export class NishiliBackendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+  }
+}
