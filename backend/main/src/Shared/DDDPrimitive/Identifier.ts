c169c3a43cff7bb4318126cce3299aa52972efcd
hirohata <hirohatatro@gmail.com>
2023-10-04 17:14:57 -0700

    update identifier

---
+++ backend/main/src/Shared/DDDPrimitive/Identifier.ts
@@ -0,0 +1,15 @@
+/**
+ * Interface of the identifier.
+ * The ID is not determined to be only one.
+ * It can be more than single element because the ID will be a combination of some elements.
+ */
+abstract class Identifier<T> {
+    private _id: T
+    constructor (id: T) {
+        this._id = id;
+    }
+
+    get id(): T {
+        return this._id;
+    }
+}
