0108e4ba2ad4c4d47a7b358b8db263f357b3fac9
hirohata <hirohatatro@gmail.com>
2023-10-05 13:21:04 -0700

    update the value object

--- backend/main/src/Shared/DDDPrimitive/ValueObject.ts
+++ backend/main/src/Shared/DDDPrimitive/ValueObject.ts
@@ -1,15 +1,28 @@
-import {Result} from "../Types/Result";
-
 interface ValueObjectProps {
     [index: string]: any;
 }
 
+/**
+ * This is the value object class.
+ * All fields are public but readonly.
+ */
 export abstract class ValueObject<T extends ValueObjectProps> {
     readonly props: T
 
-    private constructor(props: T) {
+    protected constructor(props: T) {
         this.props = props;
     }
 
-    abstract create(props: T): Result<this>
+    public equal(otherValueObject: ValueObject<T>): boolean {
+        if (otherValueObject === null || otherValueObject === undefined) {
+            return false
+        }
+
+        if (otherValueObject.props === null || otherValueObject.props === undefined) {
+            return false
+        }
+
+        return JSON.stringify(this.props) === JSON.stringify(otherValueObject.props)
+    }
+
 }
---
+++ backend/main/test/Shared/DDDPrimitives/ValueObject.test.ts
@@ -0,0 +1,52 @@
+import {describe, test, expect, it, Test} from "vitest";
+import {ValueObject} from "../../../src/Shared/DDDPrimitive";
+import {Ok, Result} from "../../../src/Shared/Types/Result";
+
+interface ITestValue {
+    "String": string,
+    "Number": number
+    "Nest": {
+        "String": string,
+        "Number": number
+    }
+}
+
+class TestValueObject extends ValueObject<ITestValue> {
+    static create(props: ITestValue): Result<TestValueObject> {
+        return Ok(new TestValueObject(props))
+    }
+}
+
+describe("Value object primitive", () => {
+
+    const valueObjectProps: ITestValue = {
+        "String": "test",
+        "Number": 42,
+        "Nest": {
+            "String": "test2",
+            "Number": 24
+        }
+    };
+
+    const testValueObject = TestValueObject.create(valueObjectProps);
+
+    it("construct", () => {
+        expect(testValueObject).toBeTruthy();
+    });
+
+    it("equal", () => {
+        const modValueObjectProps = JSON.parse(JSON.stringify(valueObjectProps));
+        modValueObjectProps.Nest.String = "tset"
+
+        const sameValue = TestValueObject.create(valueObjectProps);
+        const differentValue = TestValueObject.create(modValueObjectProps);
+
+        if (testValueObject.ok && sameValue.ok && differentValue.ok) {
+            expect(testValueObject.value.equal(sameValue.value)).toBeTruthy();
+            expect(testValueObject.value.equal(differentValue.value)).toBeFalsy();
+        } else {
+            expect(false).toBeTruthy();
+        }
+
+    })
+})
