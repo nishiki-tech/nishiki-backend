a907ccb417b2c4dc791d0c7e29d4ec4e3548d4df
hirohata <hirohatatro@gmail.com>
2023-10-04 19:30:43 -0700

    add an Entity class

---
+++ backend/main/src/Shared/DDDPrimitive/Entity.ts
@@ -0,0 +1,39 @@
+import { Identifier } from "./Identifier";
+import {Result} from "../Types/Result";
+
+/**
+ * This is an entity class.
+ * This class is used to represent the entity object in the DDD context.
+ */
+export abstract class Entity<T, K> {
+    private readonly _id: Identifier<T>;
+    private _props: K
+
+    /**
+     * The constructor is private.
+     * To create an instance, call a crete method.
+     * @param id
+     * @param props
+     * @private
+     */
+    private constructor (id: Identifier<T>, props: K) {
+        this._id = id;
+        this._props = props;
+    }
+
+    /**
+     * return this ID
+     */
+    get id(): Identifier<T> {
+        return this._id;
+    }
+
+    /**
+     * This method is used to create an instance.
+     * Inside this method, you write a validation of the input value.
+     * If all validation is passed, call the constructor.
+     * @param id
+     * @param props
+     */
+    abstract create(id: Identifier<T>, props: K): Result<Entity<T, K>>;
+}
--- backend/main/src/Shared/DDDPrimitive/index.ts
+++ backend/main/src/Shared/DDDPrimitive/index.ts
@@ -1,1 +1,2 @@
 export { Identifier } from "./Identifier";
+export { Entity } from "./Entity"
