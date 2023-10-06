e65a301c5dde8974f27c75be83d2ea01dfc3c980
hirohata <hirohatatro@gmail.com>
2023-10-05 21:43:22 -0700

    fix import

--- backend/main/test/Shared/DDDPrimitives/ValueObject.test.ts
+++ backend/main/test/Shared/DDDPrimitives/ValueObject.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { Ok, Result, ValueObject } from "../../../src/Shared";
+import { ValueObject } from "../../../src/Shared";
 
 interface ITestValue {
     "String": string,
