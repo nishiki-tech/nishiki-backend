f5a09ff0de2ef758ba596d7bb1d74ed5aab2565d
hirohata <hirohatatro@gmail.com>
2023-10-05 22:40:28 -0700

    update an aggregate root

--- backend/main/src/Shared/DDDPrimitive/AggrigateRoot.ts
+++ backend/main/src/Shared/DDDPrimitive/AggrigateRoot.ts
@@ -1,4 +1,8 @@
 import { Entity } from "src/Shared";
 
+/**
+ * This class represents the aggregate root in DDD context.
+ * Fundamentally, this class is the same as an entity.
+ */
 export abstract class AggregateRoot<T, K> extends Entity<T, K> {
 }
