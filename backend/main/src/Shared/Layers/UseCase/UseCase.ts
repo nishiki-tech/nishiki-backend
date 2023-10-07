69e34f4849406332301a880f3abd3ed5cbbf4811
hirohata <hirohatatro@gmail.com>
2023-10-07 12:28:26 -0700

    fix export

--- backend/main/src/Shared/Layers/UseCase/UseCase.ts
+++ backend/main/src/Shared/Layers/UseCase/UseCase.ts
@@ -3,6 +3,6 @@ import {Ok, Result} from "src/Shared";
 /**
  * This is the use case interface.
  */
-interface IUseCase<IInput, IResponse, IError> {
+export interface IUseCase<IInput, IResponse, IError> {
     execute(request: IInput): Result<IResponse, IError>
 }
