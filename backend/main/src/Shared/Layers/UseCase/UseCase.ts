736cd72985a473535c50357d1795437f366075f0
hirohata <hirohatatro@gmail.com>
2023-10-08 20:16:05 -0700

    update use case

--- backend/main/src/Shared/Layers/UseCase/UseCase.ts
+++ backend/main/src/Shared/Layers/UseCase/UseCase.ts
@@ -3,6 +3,26 @@ import {Ok, Result} from "src/Shared";
 /**
  * This is the use case interface.
  */
-export interface IUseCase<IInput, IResponse, IError> {
-    execute(request: IInput): Result<IResponse, IError>
+export interface IUseCase<IInput, IResponse, UseCaseError> {
+    execute(request: IInput): Promise<Result<IResponse, UseCaseError>> | Result<IResponse, UseCaseError>
 }
+
+
+/**
+ * This is base class of use case.
+ */
+abstract class UseCaseBaseError extends Error {
+    constructor(message?: string) {
+        super(message);
+    }
+}
+
+/**
+ * This class is used to explicitly represent domain object error.
+ */
+export abstract class DomainObjectError extends UseCaseBaseError{}
+
+/**
+ * This class is used to explicitly represent Use Case error.
+ */
+export abstract class UseCaseError extends UseCaseBaseError{}
