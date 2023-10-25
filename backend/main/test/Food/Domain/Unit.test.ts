f8357d90c49c4067b3b96b39eaf0fc4c470884a8
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-10-25 02:17:59 -0700

    add: test for unit object

---
+++ backend/main/test/Food/Domain/Unit.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it, test } from "vitest";
+import { Unit } from "../../../src/Food/Domain/Unit";
+import { UnitDomainError } from "../../../src/Food/Domain/Unit";
+
+describe("Unit Object", () => {
+	describe("creating unit", () => {
+		it("success", () => {
+			const unitName = "kilogram";
+
+			const unit = Unit.create({
+				name: unitName,
+			});
+
+			expect(unit.ok).toBeTruthy();
+			expect(unit.value!.name).toBe(unitName);
+		});
+
+		it("unit name too long", () => {
+			const unitName = "12345678901"; // 11 character
+			const unit = Unit.create({
+				name: unitName,
+			});
+
+            console.log(unit)
+			expect(unit.ok).toBeFalsy();
+			expect(unit.error).toBeInstanceOf(UnitDomainError);
+		});
+	});
+});
