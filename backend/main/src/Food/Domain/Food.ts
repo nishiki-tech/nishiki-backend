8347731b4e675908bb78f824cb54005b11dfb6dd
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-10-27 02:55:17 -0700

    add: Food Entity class

---
+++ backend/main/src/Food/Domain/Food.ts
@@ -0,0 +1,67 @@
+import { Entity, Err, Identifier, Ok, Result } from "src/Shared";
+import { DomainObjectError } from "src/Shared";
+import { Unit } from "./Unit";
+import { Quantity } from "src/Group/Domain/Quantity";
+import { Expiry } from "./Expiry";
+
+interface IFoodProps {
+	name: string;
+	unit?: Unit;
+	quantity?: Quantity;
+	expiry?: Expiry;
+}
+
+export class Food extends Entity<string, IFoodProps> {
+	// food name must be shorter than or equal 50.
+	static create(id: FoodId, props: IFoodProps): Result<Food, FoodDomainError> {
+		if (props.name.length > 50) {
+			return Err(new FoodDomainError("Food name is too long"));
+		}
+		return Ok(
+			new Food(id, {
+				...props,
+			}),
+		);
+	}
+
+	get name(): string {
+		return this.props.name;
+	}
+
+	get unit(): Unit | undefined {
+		return this.props?.unit;
+	}
+
+	get quantity(): Quantity | undefined {
+		return this.props?.quantity;
+	}
+
+	get expiry(): Expiry | undefined {
+		return this.props?.expiry;
+	}
+
+	public changeFoodName(argName: string): Result<Food, FoodDomainError> {
+		const {name, ...nameExcludedProps} = this.props;
+		return Food.create(this.id, {
+			...nameExcludedProps,
+			name: argName,
+		});
+	}
+
+	public changeExpiry(argExpiry: Expiry): Result<Food, FoodDomainError> {
+		const {expiry, ...expiryExcludedProps} = this.props;
+		return Food.create(this.id, {
+			...expiryExcludedProps,
+			expiry: argExpiry,
+		});
+	}
+}
+
+export class FoodId extends Identifier<string> {
+	static create(id: string): Result<FoodId, FoodIdDomainError> {
+		return Ok(new FoodId(id));
+	}
+}
+
+export class FoodIdDomainError extends DomainObjectError {}
+export class FoodDomainError extends FoodIdDomainError {}
