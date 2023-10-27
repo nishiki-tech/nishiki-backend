278789fc7a7c68bcab5fe3d37a09006a6ec6cc1d
hirohata <hirohatatro@gmail.com>
2023-10-27 13:58:34 -0700

    feat: update user DTO

--- backend/main/src/User/Dtos/UserDto.ts
+++ backend/main/src/User/Dtos/UserDto.ts
@@ -3,13 +3,11 @@ import { User } from "src/User/Domain/User";
 export interface IUserDto {
 	id: string;
 	name: string;
-	isAdmin: boolean;
 }
 
 export const userDtoMapper = (user: User): IUserDto => {
 	return {
 		id: user.id.id,
 		name: user.name,
-		isAdmin: user.isAdmin,
 	};
 };
