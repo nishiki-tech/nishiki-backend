18b3556cc4888e7ab5bad3591c16d7f7d5bcb621
hirohata <hirohatatro@gmail.com>
2023-10-31 03:25:17 -0700

    feature: added username object

---
+++ backend/main/src/User/Domain/ValueObject/Username.ts
@@ -0,0 +1,46 @@
+import { DomainObjectError, Err, Ok, Result, ValueObject } from "src/Shared";
+
+const DEFAULT_NAME = "Nishiki User";
+
+interface IUserNameProps {
+	name: string;
+}
+
+/**
+ * The Username value object.
+ */
+export class Username extends ValueObject<IUserNameProps> {
+	get name(): string {
+		return this.props.name;
+	}
+
+	/**
+	 * change username
+	 * @param name
+	 */
+	public changeName(name: string): Result<Username, UserNameDomainError> {
+		return Username.create(name);
+	}
+
+	/**
+	 * The username length must be less than equal to 30 and greater than equal to 1.
+	 * Also, if the username is not provided, the username is set to the default name, Nishiki User.
+	 * @param name
+	 */
+	static create(name?: string): Result<Username, UserNameDomainError> {
+		// return default name
+		if (name !== "" && !name) return Ok(new Username({ name: DEFAULT_NAME }));
+
+		if (name.length < 1) {
+			return Err(new UserNameDomainError("Username is too short"));
+		}
+
+		if (name.length > 30) {
+			return Err(new UserNameDomainError("Username is too long"));
+		}
+
+		return Ok(new Username({ name }));
+	}
+}
+
+export class UserNameDomainError extends DomainObjectError {}
