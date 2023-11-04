7a6329be5290ac42b504922616ccbbe207196e4c
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-11-04 02:23:02 -0700

    fix: remove duplicated lines

--- backend/main/test/Group/Domain/Entities/Container.test.ts
+++ backend/main/test/Group/Domain/Entities/Container.test.ts
@@ -10,44 +10,6 @@ import { Unit } from "../../../../src/Group/Domain/ValueObjects/Unit";
 import { Food, FoodId } from "../../../../src/Group/Domain/Entities/Food";
 
 describe("Container Object", () => {
-	describe("creating container object", () => {
-		const unit = Unit.create({ name: "g" });
-		const quantity = Quantity.create(100);
-		const expiry = Expiry.create({ date: new Date(2023, 11, 1) });
-		const foodId = FoodId.create("foodId");
-		const foodProps = {
-			name: "dummy container name",
-			unit: unit,
-			quantity: quantity,
-			expiry: expiry,
-		};
-		const food = Food.create(foodId, foodProps).value!;
-
-		const containerId = ContainerId.create("containerId");
-		const containerProps = {
-			name: "dummy container name",
-			foods: [food],
-		};
-
-		it("success", () => {
-			const container = Container.create(containerId, {
-				...containerProps,
-			});
-			expect(container.ok).toBeTruthy();
-			expect(container.value!.name).toBe(containerProps.name);
-		});
-
-		it("container name too long", () => {
-			const container = Container.create(containerId, {
-				...containerProps,
-				name: "abcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqrstuv", // 256 characters
-			});
-			expect(container.ok).toBeFalsy();
-		});
-	});
-});
-
-describe("Container Object", () => {
 	const unit = Unit.create({ name: "g" });
 	const quantity = Quantity.create(100);
 	const expiry = Expiry.create({ date: new Date(2023, 11, 1) });
@@ -65,49 +27,79 @@ describe("Container Object", () => {
 		name: "dummy container name",
 		foods: [food],
 	};
-	const container = Container.create(containerId, {
-		...containerProps,
-	}).value!;
+	describe("Container Object Constructor", () => {
+		describe("creating container object", () => {
+			it("success", () => {
+				const container = Container.create(containerId, {
+					...containerProps,
+				});
+				expect(container.ok).toBeTruthy();
+				expect(container.value!.name).toBe(containerProps.name);
+			});
 
-	describe("change container name", () => {
-		it("change container name", () => {
-			const changedContainerName = "changedContainerName";
-			const changedContainer =
-				container.changeName(changedContainerName).value!;
-			expect(changedContainer.name).toBe(changedContainerName);
-		});
-		it("change container name with too short name", () => {
-			const changedContainerName = "";
-			const changedContainer = container.changeName(changedContainerName);
-			expect(changedContainer.error).instanceOf(ContainerDomainError);
+			it("container name too long", () => {
+				const container = Container.create(containerId, {
+					...containerProps,
+					name: "abcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqlstuvwxyzabcdefghijklmnopqrstuv", // 256 characters
+				});
+				expect(container.ok).toBeFalsy();
+			});
+
+			it("container name too short", () => {
+				const container = Container.create(containerId, {
+					...containerProps,
+					name: "",
+				});
+				expect(container.ok).toBeFalsy();
+			});
 		});
 	});
 
-	describe("change container foods", () => {
-		it("add container food", () => {
-			const extraFood = Food.create("extra food id", {
-				...foodProps,
-				name: "extra food",
-			}).value!;
+	describe("Container Object methods", () => {
+		const container = Container.create(containerId, {
+			...containerProps,
+		}).value!;
 
-			const changedContainer = container.addFood(extraFood).value!;
-			expect(changedContainer.foods).toMatchObject([food, extraFood]);
+		describe("change container name", () => {
+			it("change container name", () => {
+				const changedContainerName = "changedContainerName";
+				const changedContainer =
+					container.changeName(changedContainerName).value!;
+				expect(changedContainer.name).toBe(changedContainerName);
+			});
+			it("change container name with too short name", () => {
+				const changedContainerName = "";
+				const changedContainer = container.changeName(changedContainerName);
+				expect(changedContainer.error).instanceOf(ContainerDomainError);
+			});
 		});
 
-		it("attempt to add existing food", () => {
-			const changedContainer = container.addFood(food);
-			expect(changedContainer.error).instanceOf(ContainerDomainError);
-		});
+		describe("change container foods", () => {
+			it("add container food", () => {
+				const extraFood = Food.create("extra food id", {
+					...foodProps,
+					name: "extra food",
+				}).value!;
 
-		it("remove container food", () => {
-			const changedContainer = container.removeFood(food.id).value!;
-			expect(changedContainer.foods).toMatchObject([]);
-		});
+				const changedContainer = container.addFood(extraFood).value!;
+				expect(changedContainer.foods).toMatchObject([food, extraFood]);
+			});
 
-		it("attempt to remove food which isn't included", () => {
-			const extraFoodId = FoodId.create("extra food id").value!;
-			const changedContainer = container.removeFood(extraFoodId.id);
-			expect(changedContainer.error).instanceOf(ContainerDomainError);
+			it("attempt to add existing food", () => {
+				const changedContainer = container.addFood(food);
+				expect(changedContainer.error).instanceOf(ContainerDomainError);
+			});
+
+			it("remove container food", () => {
+				const changedContainer = container.removeFood(food.id).value!;
+				expect(changedContainer.foods).toMatchObject([]);
+			});
+
+			it("attempt to remove food which isn't included", () => {
+				const extraFoodId = FoodId.create("extra food id").value!;
+				const changedContainer = container.removeFood(extraFoodId.id);
+				expect(changedContainer.error).instanceOf(ContainerDomainError);
+			});
 		});
 	});
 });
