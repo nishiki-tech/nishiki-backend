988b3e789cadd7eb506ba4f83bc4a4af92c8f137
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-11-07 02:19:00 -0800

    feat: add Group Name test

---
+++ backend/main/test/Group/Domain/ValueObjects/GroupName.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from "vitest";
+import { GroupName } from "../../../../src/Group/Domain/ValueObjects/GroupName";
+
+describe("GroupName", () => {
+	describe("Crate GroupName Object", () => {
+		it("success ", () => {
+			const groupNameString = "GroupName";
+			const groupName = GroupName.create(groupNameString);
+
+			expect(groupName.ok).toBeTruthy();
+			expect(groupName.value.name).toBe(groupNameString);
+		});
+
+		it("if the GroupName is not provided, the GroupName is set to the default name, Nishiki User", () => {
+			const groupName = GroupName.create();
+
+			expect(groupName.ok).toBeTruthy();
+			expect(groupName.value.name).toBe("Nishiki Group");
+		});
+
+		it("the GroupName is too short", () => {
+			const groupName = GroupName.create("");
+			expect(groupName.ok).toBeFalsy();
+		});
+
+		it("the GroupName is too long, greater than 255", () => {
+			// set the string with a length of 256.
+			const groupName = GroupName.create("a".repeat(256));
+			expect(groupName.ok).toBeFalsy();
+		});
+	});
+
+	describe("change GroupName", () => {
+		it("success", () => {
+			const groupName = GroupName.create("Name").value;
+			const updated = groupName.changeName("emaN");
+
+			expect(updated.ok).toBeTruthy();
+			expect(updated.value.name).toBe("emaN");
+		});
+	});
+});
