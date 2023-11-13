6d31acb56ca71883af90962176a0b69dd94e0305
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-11-12 23:48:19 -0800

    fix: update testcoes

--- backend/main/test/Group/Domain/Entities/Group.test.ts
+++ backend/main/test/Group/Domain/Entities/Group.test.ts
@@ -1,124 +1,81 @@
 import { describe, expect, it } from "vitest";
-import { Group, GroupId } from "../../../../src/Group/Domain/Entities/Group";
-import { GroupDomainError } from "../../../../src/Group/Domain/Entities/Group";
-import { Quantity } from "../../../../src/Group/Domain/ValueObjects/Quantity";
-import { Expiry } from "../../../../src/Group/Domain/ValueObjects/Expiry";
-import { Unit } from "../../../../src/Group/Domain/ValueObjects/Unit";
-import { Food, FoodId } from "../../../../src/Group/Domain/Entities/Food";
+import { Group, GroupId } from "src/Group/Domain/Entities/Group";
+import { GroupDomainError } from "src/Group/Domain/Entities/Group";
 import {
-	Container,
 	ContainerId,
-} from "../../../../src/Group/Domain/Entities/Container";
-import { User, UserId } from "../../../../src/User";
-import { EmailAddress } from "../../../../src/User/Domain/ValueObject/EmailAddress";
-import { Username } from "../../../../src/User/Domain/ValueObject/Username";
+} from "src/Group/Domain/Entities/Container";
+import { UserId } from "src/User";
 
 describe("Group Object", () => {
-	const unit = Unit.create({ name: "g" }).value!;
-	const quantity = Quantity.create(100).value!;
-	const expiry = Expiry.create({ date: new Date(2023, 11, 1) }).value!;
-	const foodId = FoodId.create("foodId").value!;
-	const anotherFoodId = FoodId.create("anotherFoodId").value!;
-	const foodProps = {
-		name: "dummy food name",
-		unit: unit,
-		quantity: quantity,
-		expiry: expiry,
-	};
-	const food = Food.create(foodId, foodProps).value!;
-	const anotherFood = Food.create(anotherFoodId, foodProps).value!;
-
-	const containerId = ContainerId.create("containerId").value!;
-	const anotherContainerId = ContainerId.create("anotherContainerId").value!;
-	const containerProps = {
-		name: "dummy container name",
-		foods: [food, anotherFood],
-	};
-	const container = Container.create(containerId, containerProps).value!;
-	const anotherContainer = Container.create(anotherContainerId, containerProps)
-		.value!;
+	const containerId = ContainerId.create("containerId").unwrap();
+	const anotherContainerId = ContainerId.create("anotherContainerId").unwrap();
 
 	const userId = UserId.generate()!;
 	const anotherUserId = UserId.generate()!;
-	const username = Username.create("dummy user name");
-	const emailAddress = EmailAddress.create("bar@nishiki.com").value;
-	const user = User.create(userId, {
-		username,
-		emailAddress,
-	});
-	const anotherUser = User.create(anotherUserId, {
-		username,
-		emailAddress,
-	});
 
-	const groupId = GroupId.create("GroupId");
+	const groupId = GroupId.create("GroupId").unwrap();
 	const GroupProps = {
 		name: "dummy Group name",
-		containers: [container, anotherContainer],
-		users: [user, anotherUser],
+		containerIds: [containerId, anotherContainerId],
+		userIds: [userId, anotherUserId],
 	};
 
 	describe("Group Object Constructor", () => {
 		describe("creating Group object", () => {
 			it("success", () => {
-				const group = Group.create(GroupId, {
+				const group = Group.create(groupId, {
 					...GroupProps,
 				});
 				expect(group.ok).toBeTruthy();
-				expect(group.value!.name).toBe(GroupProps.name);
+				expect(group.unwrap().name).toBe(GroupProps.name);
 			});
 		});
 	});
 
 	describe("Group Object methods", () => {
-		const group = Group.create(GroupId, {
+		const group = Group.create(groupId, {
 			...GroupProps,
-		}).value!;
+		}).unwrap();
 
 		describe("change Group name", () => {
 			it("change Group name", () => {
 				const changedGroupName = "changedGroupName";
-				const changedGroup = group.changeName(changedGroupName).value!;
+				const changedGroup = group.changeName(changedGroupName).unwrap();
 				expect(changedGroup.name).toBe(changedGroupName);
 			});
 			it("change Group name with too short name", () => {
 				const changedGroupName = "";
 				const changedGroup = group.changeName(changedGroupName);
-				expect(changedGroup.error).instanceOf(GroupDomainError);
+				expect(changedGroup.unwrapError()).instanceOf(GroupDomainError);
 			});
 		});
 
-		describe("change Group containers", () => {
+		describe("change Group containerIds", () => {
 			it("add Group container", () => {
-				const extraContainer = Container.create("extra container id", {
-					...containerProps,
-					name: "extra container",
-				}).value!;
+				const extraContainerId = ContainerId.create("extra container id").unwrap();
 
-				const changedGroup = group.addContainer(extraContainer).value!;
-				expect(changedGroup.containers).toMatchObject([
-					...group.containers,
-					extraContainer,
+				const changedGroup = group.addContainerId(extraContainerId).unwrap();
+				expect(changedGroup.containerIds).toMatchObject([
+					...group.containerIds,
+					extraContainerId,
 				]);
 			});
 
 			it("attempt to add existing container", () => {
-				const changedGroup = group.addContainer(container);
-				expect(changedGroup.error).instanceOf(GroupDomainError);
+				const changedGroup = group.addContainerId(containerId);
+				expect(changedGroup.unwrapError()).instanceOf(GroupDomainError);
 			});
 
 			it("remove Group container", () => {
-				const changedGroup = group.removeContainer(container.id).value!;
-				console.log("changedGroup", changedGroup.containers);
-
-				expect(changedGroup.containers).toMatchObject([anotherContainer]);
+				const changedGroup = group.removeContainerId(containerId).unwrap();
+				expect(changedGroup.containerIds).toMatchObject([anotherContainerId]);
 			});
 
 			it("attempt to remove container which isn't included", () => {
 				const extraContainerId =
-					ContainerId.create("extra container id").value!;
-				const changedGroup = group.removeContainer(extraContainerId.id);
-				expect(changedGroup.error).instanceOf(GroupDomainError);
+					ContainerId.create("extra container id").unwrap();
+				const changedGroup = group.removeContainerId(extraContainerId);				
+				expect(changedGroup.unwrapError()).instanceOf(GroupDomainError);
 			});
 		});
 	});
