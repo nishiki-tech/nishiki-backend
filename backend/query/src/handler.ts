1a77037dc744e69c032fee2a6e96a253ee760d5f
hirohata <43802663+hitohata@users.noreply.github.com>
2023-11-17 00:23:06 +0900

    feature: added and initialized the query project (#98)

---
+++ backend/query/src/handler.ts
@@ -0,0 +1,12 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (
+	event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: "test",
+		}),
+	};
+};
