77df3fb2ec28f6ef3b3ef07caa4716e4e084f446
hirohata <hirohatatro@gmail.com>
2023-11-22 09:44:35 -0800

    feature: added helper functions and type.

---
+++ cdk/utils/helpers.ts
@@ -0,0 +1,36 @@
+import { Stage } from "./nishiki-backend-resource-types"
+
+/**
+ * This function returns stage name based on the environment variable.
+ * If the environment variable is not set, it returns 'dev'.
+ * If the environment variable is set to 'prod', it returns 'prod'.
+ * If the environment variable is set to 'dev' or 'prod' or undefined, it throws error.
+ */
+export const stageName = (): Stage => {
+    // undefined or 'dev'.
+    if (!process.env.STAGE || process.env.STAGE === "dev") {
+        return "dev"
+    }
+
+    if (process.env.STAGE === "prod") {
+        return "prod"
+    }
+
+    throw new Error("STAGE is not set or set to invalid value.")
+}
+
+/**
+ * This function takes a resource name and the stage name.
+ * If the stage name is not provided or 'dev', it returns the resource name with '-dev' suffix.
+ * If the stage name is 'prod', it just returns the resource name.
+ * @param {string} resourceName - The name of the resource.
+ * @param {Stage} stageName - The name of the stage.
+ * @returns {string} - The name of the resource with the stage suffix.
+ */
+export const resourceName = (resourceName: string, stageName?: Stage): string => {
+	if (stageName === "prod") {
+		return resourceName;
+	} else {
+		return resourceName + "-dev";
+	}
+}
