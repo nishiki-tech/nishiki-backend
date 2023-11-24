79032a0787df8c4877e4d9945c924307502a291e
hirohata <hirohatatro@gmail.com>
2023-11-23 19:40:05 -0800

    feature: added a DynamoDB client

---
+++ backend/main/src/Shared/Adapters/DynamoClient.ts
@@ -0,0 +1,8 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {REGION} from "src/Settings/Setting";
+
+const dynamoConfig = {
+    region: REGION,
+}
+
+export const dynamoClient = new DynamoDBClient(dynamoConfig);
