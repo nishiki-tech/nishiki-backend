d75c406339d8536015faf7e5d8a2ce95ea3a9fa3
hirohata <43802663+hitohata@users.noreply.github.com>
2023-11-28 21:41:11 -0800

    Feature/109 create a dynamodb resource in the cdk (#111)
    * feature: added helper functions and type.
    
    * feature: updated a comment in the helper function.
    
    * feature: added DynamoDB resource
    
    * chore: format
    
    * feature: added GSIs
    
    * feature: updated a removal policy
    
    * chore: format

---
+++ cdk/lib/nishikiStaticAssets-stack.ts
@@ -0,0 +1,67 @@
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+	AttributeType,
+	BillingMode,
+	ProjectionType,
+	Table,
+} from "aws-cdk-lib/aws-dynamodb";
+import { resourceName, Stage } from "../utils";
+
+/**
+ * If the stage is not prod, add "-dev" to the every asset's name.
+ */
+interface IProps extends StackProps {
+	readonly stage: Stage;
+}
+
+export class NishikiStaticAssetsStack extends Stack {
+	public readonly nishikiTable: Table;
+
+	constructor(scope: Construct, id: string, props: IProps) {
+		super(scope, id, props);
+
+		const { stage } = props;
+
+		const nishikiTable = new Table(this, "NishikiTable", {
+			tableName: resourceName("NishikiTable", stage),
+			billingMode: BillingMode.PAY_PER_REQUEST,
+			partitionKey: {
+				name: "PK",
+				type: AttributeType.STRING,
+			},
+			sortKey: {
+				name: "SK",
+				type: AttributeType.STRING,
+			},
+			removalPolicy:
+				stage === "prod" ? RemovalPolicy.RETAIN : RemovalPolicy.DESTROY,
+		});
+
+		nishikiTable.addGlobalSecondaryIndex({
+			indexName: "UserAndGroupRelationship",
+			partitionKey: {
+				name: "GroupId",
+				type: AttributeType.STRING,
+			},
+			sortKey: {
+				name: "UserId",
+				type: AttributeType.STRING,
+			},
+			projectionType: ProjectionType.KEYS_ONLY,
+		});
+
+		nishikiTable.addGlobalSecondaryIndex({
+			indexName: "JoinLink",
+			partitionKey: {
+				name: "GroupId",
+				type: AttributeType.STRING,
+			},
+			sortKey: {
+				name: "LinkExpiredDatetime",
+				type: AttributeType.STRING,
+			},
+			projectionType: ProjectionType.KEYS_ONLY,
+		});
+	}
+}
---
+++ cdk/test/nishikiStaticAssets-stack.test.ts
@@ -0,0 +1,116 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { NishikiStaticAssetsStack } from "../lib/nishikiStaticAssets-stack";
+import { App } from "aws-cdk-lib";
+
+describe("Static Assets", () => {
+	const app = new App();
+	const stack = new NishikiStaticAssetsStack(app, "stack", { stage: "prod" });
+	const template = Template.fromStack(stack);
+
+	test("DynamoDB", () => {
+		template.resourceCountIs("AWS::DynamoDB::Table", 1);
+
+		template.hasResource("AWS::DynamoDB::Table", {
+			UpdateReplacePolicy: "Retain",
+			DeletionPolicy: "Retain",
+		});
+
+		template.hasResourceProperties("AWS::DynamoDB::Table", {
+			TableName: "NishikiTable",
+			BillingMode: "PAY_PER_REQUEST",
+		});
+
+		template.hasResourceProperties("AWS::DynamoDB::Table", {
+			AttributeDefinitions: [
+				{
+					AttributeName: "PK",
+					AttributeType: "S",
+				},
+				{
+					AttributeName: "SK",
+					AttributeType: "S",
+				},
+				{
+					AttributeName: "GroupId",
+					AttributeType: "S",
+				},
+				{
+					AttributeName: "UserId",
+					AttributeType: "S",
+				},
+				{
+					AttributeName: "LinkExpiredDatetime",
+					AttributeType: "S",
+				},
+			],
+			KeySchema: [
+				{
+					AttributeName: "PK",
+					KeyType: "HASH",
+				},
+				{
+					AttributeName: "SK",
+					KeyType: "RANGE",
+				},
+			],
+		});
+
+		// check GSI
+		template.hasResourceProperties("AWS::DynamoDB::Table", {
+			GlobalSecondaryIndexes: [
+				{
+					IndexName: "UserAndGroupRelationship",
+					KeySchema: [
+						{
+							AttributeName: "GroupId",
+							KeyType: "HASH",
+						},
+						{
+							AttributeName: "UserId",
+							KeyType: "RANGE",
+						},
+					],
+					Projection: {
+						ProjectionType: "KEYS_ONLY",
+					},
+				},
+				{
+					IndexName: "JoinLink",
+					KeySchema: [
+						{
+							AttributeName: "GroupId",
+							KeyType: "HASH",
+						},
+						{
+							AttributeName: "LinkExpiredDatetime",
+							KeyType: "RANGE",
+						},
+					],
+					Projection: {
+						ProjectionType: "KEYS_ONLY",
+					},
+				},
+			],
+		});
+	});
+});
+
+describe("dev environment", () => {
+	const app = new cdk.App();
+	const stack = new NishikiStaticAssetsStack(app, "dev-stack", {
+		stage: "dev",
+	});
+	const template = Template.fromStack(stack);
+
+	template.hasResource("AWS::DynamoDB::Table", {
+		UpdateReplacePolicy: "Delete",
+		DeletionPolicy: "Delete",
+	});
+
+	test("DynamoDB name", () => {
+		template.hasResourceProperties("AWS::DynamoDB::Table", {
+			TableName: "NishikiTable-dev",
+		});
+	});
+});
