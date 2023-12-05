e4d9131e07754aa6a739d88104ba494eabd5bffb
hirohata <hirohatatro@gmail.com>
2023-12-05 16:21:35 +0900

    fix: fixed stack names

--- cdk/bin/nishikiBackend.ts
+++ cdk/bin/nishikiBackend.ts
@@ -3,16 +3,16 @@ import * as cdk from "aws-cdk-lib";
 import "source-map-support/register";
 import { NishikiBackendStack } from "../lib/nishikiBackend-stack";
 import { NishikiStaticAssetsStack } from "../lib/nishikiStaticAssets-stack";
-import { stageName } from "../utils";
+import {resourceName, stageName} from "../utils";
 
 const stage = stageName();
 
 const app = new cdk.App();
 const staticAssets = new NishikiStaticAssetsStack(
 	app,
-	`NishikiStaticAssetsStack-${stage}`,
+	resourceName("NishikiStaticAssetsStack", stage),
 	{ stage },
 );
-new NishikiBackendStack(app, `NishikiBackendStack-${stage}`, {
+new NishikiBackendStack(app, resourceName("NishikiBackendStack", stage), {
 	stage,
 });
