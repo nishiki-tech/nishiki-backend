ef1700b6ed6afb21207835d407b0469819695ef0
hirohata <43802663+hitohata@users.noreply.github.com>
2023-12-07 05:18:58 +0900

    setup test environment (#125)
    * feat: added a new script.
    
    * feat: updated the initializer to wait for the creation of DynamoDB.
    
    * feat: added the command in the docker compose and the condition.
    
    * feat: changed the way of running the test.
    
    * feat: updated the initializer script.
    
    * feat: added a delete command in the initializer
    
    * chore: remove an unnecessary log.
    
    * fix: fixed test command
    
    * chore: format

--- backend/main/others/dynamodbInitializer/src/index.ts
+++ backend/main/others/dynamodbInitializer/src/index.ts
@@ -2,6 +2,8 @@ import {
 	DynamoDBClient,
 	CreateTableCommand,
 	CreateTableCommandInput,
+	ResourceInUseException,
+	DeleteTableCommand,
 } from "@aws-sdk/client-dynamodb";
 import { DynamoDBClientConfig } from "@aws-sdk/client-dynamodb/dist-types/DynamoDBClient";
 
@@ -9,6 +11,8 @@ const config: DynamoDBClientConfig = {
 	region: process.env.AWS_REGION,
 	endpoint: process.env.DYNAMO_ENDPINT!,
 };
+
+const TABLE_NAME = "Nishiki-DB";
 
 const client = new DynamoDBClient(config);
 
@@ -87,7 +91,7 @@ const input: CreateTableCommandInput = {
 			},
 		},
 	],
-	TableName: "Nishiki-DB",
+	TableName: TABLE_NAME,
 	ProvisionedThroughput: {
 		ReadCapacityUnits: 2,
 		WriteCapacityUnits: 2,
@@ -98,12 +102,51 @@ const input: CreateTableCommandInput = {
 
 const command = new CreateTableCommand(input);
 
-client
-	.send(command)
-	.then((req) => {
-		console.log(req);
+const runCommand = async () => {
+	let retry_limit = 3;
+
+	while (retry_limit > 0) {
+		console.log("initializing...");
+		try {
+			const req = await client.send(command);
+			console.log(req);
+			break;
+		} catch (err) {
+			retry_limit--;
+
+			if (err instanceof ResourceInUseException) {
+				console.log("already exists");
+				const deleteCommand = new DeleteTableCommand({ TableName: TABLE_NAME });
+				await client.send(deleteCommand);
+				continue;
+			}
+
+			console.error(err);
+
+			await new Promise(() => setTimeout(() => {}, 1000));
+
+			console.log("retrying...");
+
+			if (retry_limit === 0) {
+				if (err instanceof Error) {
+					throw err;
+				}
+
+				if (typeof err === "string") {
+					throw Error(err);
+				}
+
+				console.error("initializing failed");
+			}
+		}
+	}
+};
+
+// run async function
+runCommand()
+	.then(() => {
+		process.exit();
 	})
-	.catch((err) => {
-		console.error(err);
-		throw Error("initializing table failed.");
+	.catch(() => {
+		process.exit(1);
 	});
