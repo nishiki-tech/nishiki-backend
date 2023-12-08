c9e28a40fcaa335eb3cc83c47bd03bf9ed099ecc
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-08 01:12:30 -0800

    feat: add delete container test

---
+++ backend/main/test/Group/UseCase/DeleteContainerUseCase.test.ts
@@ -0,0 +1,28 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { DeleteContainerUseCase } from "src/Group/UseCases/DeleteContainerUseCase/DeleteContainerUseCase";
+import { MockContainerRepository } from "../MockContainerRepository";
+import { Container, ContainerId } from "src/Group/Domain/Entities/Container";
+
+describe("delete container use case", () => {
+	let mockContainerRepository: MockContainerRepository;
+	let useCase: DeleteContainerUseCase;
+
+	beforeEach(() => {
+		mockContainerRepository = new MockContainerRepository();
+		useCase = new DeleteContainerUseCase(mockContainerRepository);
+	});
+
+	afterEach(() => {
+		vi.clearAllMocks();
+	});
+
+	it("Delete container", async () => {
+		const containerId = ContainerId.create("dummyId").unwrap();
+		const container: Container = Container.default(containerId).unwrap();
+		// add the mock container into the mock repo.
+		mockContainerRepository.pushDummyData(container);
+
+		const result = await useCase.execute({ id: "dummyId" });
+		expect(result.ok).toBeTruthy();
+	});
+});
