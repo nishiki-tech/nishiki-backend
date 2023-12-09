e625da27403499babb56f155bedeacfedd1d9356
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-09 02:03:56 -0800

    feat: add controller for add food to container usecase

---
+++ backend/main/src/Group/Controllers/AddFoodToContainerController.ts
@@ -0,0 +1,28 @@
+import { Controller } from "src/Shared";
+import { AddFoodToContainerUseCase } from "src/Group/UseCases/AddFoodToContainerUseCase/AddFoodToContainerUseCase";
+
+interface IAddFoodToContainerInput {
+	containerId: string;
+	name: string;
+	unit?: string;
+	quantity?: number;
+	expiry?: Date;
+}
+
+export class AddFoodToContainerController extends Controller<IAddFoodToContainerInput> {
+	readonly useCase: AddFoodToContainerUseCase;
+
+	constructor(useCase: AddFoodToContainerUseCase) {
+		super();
+		this.useCase = useCase;
+	}
+	protected async handler(input: IAddFoodToContainerInput) {
+		const result = await this.useCase.execute({ ...input });
+
+		if (!result.ok) {
+			return this.badRequest(result.error.message);
+		}
+
+		return this.accepted();
+	}
+}
