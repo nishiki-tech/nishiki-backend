9f31b7224052134b2efa2e2e5ead9c5c355fad16
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-09 01:29:15 -0800

    feat: add update container name contoroller

---
+++ backend/main/src/Group/Controllers/UpdateContainerNameController.ts
@@ -0,0 +1,28 @@
+import { Controller } from "src/Shared";
+import { UpdateContainerNameUseCase } from "src/Group/UseCases/UpdateContainerNameUseCase/UpdateContainerNameUseCase";
+import { IContainerDto } from "src/Group/Dtos/ContainerDto";
+
+interface IUpdateContainerNameInput {
+	containerId: string;
+	name: string;
+}
+
+export class UpdateContainerNameController extends Controller<IUpdateContainerNameInput> {
+	readonly useCase: UpdateContainerNameUseCase;
+
+	constructor(useCase: UpdateContainerNameUseCase) {
+		super();
+		this.useCase = useCase;
+	}
+	protected async handler(input: IUpdateContainerNameInput) {
+		const { containerId, name } = input;
+
+		const result = await this.useCase.execute({ containerId, name });
+
+		if (!result.ok) {
+			return this.badRequest(result.error.message);
+		}
+
+		return this.accepted();
+	}
+}
