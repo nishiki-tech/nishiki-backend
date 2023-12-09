d4ae37400296f5a663c374f74a5da01e6c34c511
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-09 01:31:50 -0800

    feat: add Add food to container usecase

---
+++ backend/main/src/Group/UseCases/AddFoodToContainerUseCase/IAddFoodToContainerUseCase.ts
@@ -0,0 +1,21 @@
+import { UseCaseError } from "src/Shared";
+import {
+	ContainerIdDomainError,
+	ContainerDomainError,
+} from "src/Group/Domain/Entities/Container";
+
+export interface IAddFoodToContainerUseCase {
+	containerId: string;
+	foodId: string;
+	name: string;
+	unit?: string;
+	quantity?: number;
+	expiry?: Date;
+}
+
+export class ContainerIsNotExisting extends UseCaseError {}
+
+export type AddFoodToContainerUseCaseErrorType =
+	| ContainerIsNotExisting
+	| ContainerIdDomainError
+	| ContainerDomainError;
