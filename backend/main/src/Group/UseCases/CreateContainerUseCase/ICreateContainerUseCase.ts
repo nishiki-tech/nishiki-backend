932c82560612ac7068994a11123a64982f4e6ca3
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-12 14:53:21 -0800

    fix: add groupId for create container Interface

--- backend/main/src/Group/UseCases/CreateContainerUseCase/ICreateContainerUseCase.ts
+++ backend/main/src/Group/UseCases/CreateContainerUseCase/ICreateContainerUseCase.ts
@@ -3,6 +3,11 @@ import { ContainerDomainError } from "src/Group/Domain/Entities/Container";
 
 export interface ICreateContainerUseCase {
 	name?: string;
+	groupId: string;
 }
 
-export type CreateContainerUseCaseErrorType = ContainerDomainError;
+export class GroupIsNotExisting extends UseCaseError {}
+
+export type CreateContainerUseCaseErrorType =
+	| ContainerDomainError
+	| GroupIsNotExisting;
