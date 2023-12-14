550cd1643aaeb6b9b8d660215545b9b4d6304e13
hirohata <hirohatatro@gmail.com>
2023-12-14 18:53:45 +0900

    chore: removed an unused file

--- backend/main/src/Shared/Adapters/DynamoClient.ts
+++
@@ -1,8 +0,0 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { REGION } from "src/Settings/Setting";
-
-const dynamoConfig = {
-	region: REGION,
-};
-
-export const dynamoClient = new DynamoDBClient(dynamoConfig);
