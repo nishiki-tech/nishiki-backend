ab84ee2b0363eb1a3909c832305fa53877219fd2
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-16 02:33:32 -0800

    fix: DeleteFoodUsecase test

--- backend/main/test/Group/UseCase/DeleteFoodFromContainerUseCase.test.ts
+++ backend/main/test/Group/UseCase/DeleteFoodFromContainerUseCase.test.ts
@@ -6,7 +6,10 @@ import {
 	ContainerDomainError,
 	ContainerId,
 } from "src/Group/Domain/Entities/Container";
-import { ContainerIsNotExisting } from "src/Group/UseCases/DeleteFoodFromContainerUseCase/IDeleteFoodFromContainerUseCase";
+import {
+	GroupIsNotExisting,
+	UserIsNotAuthorized,
+} from "src/Group/UseCases/DeleteFoodFromContainerUseCase/IDeleteFoodFromContainerUseCase";
 import { Food, FoodId } from "src/Group/Domain/Entities/Food";
 import { GroupId, Group } from "src/Group/Domain/Entities/Group";
 import { UserId } from "src/User";
@@ -20,9 +23,16 @@ describe("delete a food from container use case", () => {
 	let useCase: DeleteFoodFromContainerUseCase;
 	const userId = UserId.create(USER_ID).unwrap();
 
-	const containerId = ContainerId.create("dummyId").unwrap();
-	const container: Container = Container.default(containerId).unwrap();
 	const foodId = FoodId.create("dummyFoodId").unwrap();
+	const food = Food.create(foodId, {
+		name: "dummy food name",
+	}).unwrap();
+
+	const containerId = ContainerId.create("dummyId").unwrap();
+	const container = Container.create(containerId, {
+		name: "dummy container name",
+		foods: [food],
+	}).unwrap();
 
 	const groupId = GroupId.create("dummyGroupId").unwrap();
 	const groupName = "dummyGroupName";
@@ -46,15 +56,8 @@ describe("delete a food from container use case", () => {
 	});
 
 	it("delete a food from a exiting container", async () => {
-		const food = Food.create(foodId, {
-			name: "dummy food name",
-		}).unwrap();
-		const container: Container = Container.create(containerId, {
-			name: "dummy container name",
-			foods: [food],
-		}).unwrap();
-
 		mockContainerRepository.pushDummyData(container);
+		mockGroupRepository.pushDummyData(group);
 
 		const result = await useCase.execute({
 			userId: userId.id,
@@ -64,26 +67,33 @@ describe("delete a food from container use case", () => {
 		expect(result.ok).toBeTruthy();
 	});
 
-	it("Container not found", async () => {
+	it("Container not found in any Group", async () => {
+		const groupWithoutContainer = Group.create(groupId, {
+			name: groupName,
+			containerIds: [],
+			userIds: [userId],
+		}).unwrap();
+
+		mockContainerRepository.pushDummyData(container);
+		mockGroupRepository.pushDummyData(groupWithoutContainer);
+
 		const result = await useCase.execute({
 			userId: userId.id,
 			containerId: containerId.id,
 			foodId: foodId.id,
 		});
 		expect(result.ok).toBeFalsy();
-		expect(result.unwrapError()).instanceOf(ContainerIsNotExisting);
+		expect(result.unwrapError()).instanceOf(GroupIsNotExisting);
 	});
 
 	it("Food not found", async () => {
-		const container: Container = Container.create(containerId, {
+		const containerWithNoFood: Container = Container.create(containerId, {
 			name: "dummy container name",
 			foods: [],
 		}).unwrap();
 
-		// TODO: add the mock container into the mock repo instead of using spy.
-		vi.spyOn(mockContainerRepository, "find").mockReturnValueOnce(
-			Promise.resolve(container),
-		);
+		mockContainerRepository.pushDummyData(containerWithNoFood);
+		mockGroupRepository.pushDummyData(group);
 		const result = await useCase.execute({
 			userId: userId.id,
 			containerId: containerId.id,
@@ -91,5 +101,23 @@ describe("delete a food from container use case", () => {
 		});
 		expect(result.ok).toBeFalsy();
 		expect(result.unwrapError()).instanceOf(ContainerDomainError);
+	});
+	it("User is not authorized", async () => {
+		const USER_ID_2 = UserId.generate().id;
+		const userId_2 = UserId.create(USER_ID_2).unwrap();
+
+		mockContainerRepository.pushDummyData(container);
+		mockGroupRepository.pushDummyData(group);
+
+		const result = await useCase.execute({
+			userId: userId_2.id,
+			containerId: containerId.id,
+			foodId: foodId.id,
+		});
+		expect(result.ok).toBeFalsy();
+		expect(result.unwrapError()).instanceOf(UserIsNotAuthorized);
+		await expect(mockContainerRepository.find(containerId)).resolves.toBe(
+			container,
+		);
 	});
 });
