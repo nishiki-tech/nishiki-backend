eee52c9863a96f477d0f65d866932b5bab3fe0c9
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-16 02:25:24 -0800

    feat: user validation for FindContainerUsecase

--- backend/main/src/Group/UseCases/FindContainerUseCase/FindContainerUseCase.ts
+++ backend/main/src/Group/UseCases/FindContainerUseCase/FindContainerUseCase.ts
@@ -4,9 +4,13 @@ import { IUseCase } from "src/Shared";
 import { Err, Ok, Result } from "result-ts-type";
 import {
 	FindContainerUseCaseErrorType,
+	GroupIsNotExisting,
 	IFindContainerUseCase,
+	UserIsNotAuthorized,
 } from "src/Group/UseCases/FindContainerUseCase/IFindContainerUseCase";
 import { IContainerDto, containerDtoMapper } from "src/Group/Dtos/ContainerDto";
+import { IGroupRepository } from "src/Group/Domain/IGroupRepository";
+import { UserId } from "src/User";
 
 /**
  * Find a container.
@@ -20,21 +24,47 @@ export class FindContainerUseCase
 		>
 {
 	private readonly containerRepository: IContainerRepository;
+	private readonly groupRepository: IGroupRepository;
 
-	constructor(containerRepository: IContainerRepository) {
+	constructor(
+		containerRepository: IContainerRepository,
+		groupRepository: IGroupRepository,
+	) {
 		this.containerRepository = containerRepository;
+		this.groupRepository = groupRepository;
 	}
 
 	public async execute(
 		request: IFindContainerUseCase,
 	): Promise<Result<IContainerDto | null, FindContainerUseCaseErrorType>> {
 		const containerIdOrError = ContainerId.create(request.containerId);
-
 		if (!containerIdOrError.ok) {
 			return Err(containerIdOrError.error);
 		}
-
 		const containerId = containerIdOrError.value;
+
+		// check the user is the member of the group
+		const group = await this.groupRepository.findByContainerId(containerId);
+		if (!group) {
+			return Err(
+				new GroupIsNotExisting("The requested group is not existing."),
+			);
+		}
+		const userIdOrError = UserId.create(request.userId);
+		if (!userIdOrError.ok) {
+			return Err(userIdOrError.error);
+		}
+		const userId = userIdOrError.value;
+		const canEdit = group.canEdit(userId);
+
+		if (!canEdit) {
+			return Err(
+				new UserIsNotAuthorized(
+					"The user is not authorized to edit the container.",
+				),
+			);
+		}
+
 		const container = await this.containerRepository.find(containerId);
 
 		return Ok(container ? containerDtoMapper(container) : null);
--- backend/main/test/Group/UseCase/FindContainerUseCase.test.ts
+++ backend/main/test/Group/UseCase/FindContainerUseCase.test.ts
@@ -3,14 +3,32 @@ import { FindContainerUseCase } from "src/Group/UseCases/FindContainerUseCase/Fi
 import { MockContainerRepository } from "../MockContainerRepository";
 import { Container, ContainerId } from "src/Group/Domain/Entities/Container";
 import { containerDtoMapper } from "src/Group/Dtos/ContainerDto";
+import { UserId } from "src/User";
+import { MockGroupRepository } from "../MockGroupRepository";
+import { Group, GroupId } from "src/Group/Domain/Entities/Group";
+import { UserIsNotAuthorized } from "src/Group/UseCases/FindContainerUseCase/IFindContainerUseCase";
+
+const USER_ID = UserId.generate().id;
 
 describe("find container use case", () => {
 	let mockContainerRepository: MockContainerRepository;
+	let mockGroupRepository: MockGroupRepository;
 	let useCase: FindContainerUseCase;
+	const userId = UserId.create(USER_ID).unwrap();
+
+	const containerId = ContainerId.create("dummyId").unwrap();
+	const container: Container = Container.default(containerId).unwrap();
+
+	const groupId = GroupId.create("dummyGroupId").unwrap();
+	const groupName = "dummyGroupName";
 
 	beforeEach(() => {
 		mockContainerRepository = new MockContainerRepository();
-		useCase = new FindContainerUseCase(mockContainerRepository);
+		mockGroupRepository = new MockGroupRepository();
+		useCase = new FindContainerUseCase(
+			mockContainerRepository,
+			mockGroupRepository,
+		);
 	});
 
 	afterEach(() => {
@@ -18,21 +36,71 @@ describe("find container use case", () => {
 	});
 
 	it("Find exiting container", async () => {
-		const containerId = ContainerId.create("dummyId").unwrap();
-		const container: Container = Container.default(containerId).unwrap();
+		const group = Group.create(groupId, {
+			name: groupName,
+			containerIds: [containerId],
+			userIds: [userId],
+		}).unwrap();
 
 		vi.spyOn(mockContainerRepository, "find").mockReturnValueOnce(
 			Promise.resolve(container),
 		);
+		vi.spyOn(mockGroupRepository, "findByContainerId").mockReturnValueOnce(
+			Promise.resolve(group),
+		);
 
-		const result = await useCase.execute({ containerId: containerId.id });
+		const result = await useCase.execute({
+			userId: userId.id,
+			containerId: containerId.id,
+		});
 		expect(result.ok).toBeTruthy();
 		expect(result.unwrap()).toEqual(containerDtoMapper(container));
 	});
 
 	it("Container not found", async () => {
-		const result = await useCase.execute({ containerId: "dummyId" });
+		const group = Group.create(groupId, {
+			name: groupName,
+			containerIds: [],
+			userIds: [userId],
+		}).unwrap();
+
+		vi.spyOn(mockContainerRepository, "find").mockReturnValueOnce(
+			Promise.resolve(null),
+		);
+		vi.spyOn(mockGroupRepository, "findByContainerId").mockReturnValueOnce(
+			Promise.resolve(group),
+		);
+		const result = await useCase.execute({
+			userId: userId.id,
+			containerId: "dummyId",
+		});
 		expect(result.ok).toBeTruthy();
 		expect(result.unwrap()).toBeNull();
+	});
+	it("User is not authorized", async () => {
+		const group = Group.create(groupId, {
+			name: groupName,
+			containerIds: [containerId],
+			userIds: [userId],
+		}).unwrap();
+
+		const USER_ID_2 = UserId.generate().id;
+		const userId_2 = UserId.create(USER_ID_2).unwrap();
+
+		vi.spyOn(mockContainerRepository, "find").mockReturnValueOnce(
+			Promise.resolve(container),
+		);
+		vi.spyOn(mockGroupRepository, "findByContainerId").mockReturnValueOnce(
+			Promise.resolve(group),
+		);
+		const result = await useCase.execute({
+			userId: userId_2.id,
+			containerId: containerId.id,
+		});
+		expect(result.ok).toBeFalsy();
+		expect(result.unwrapError()).instanceOf(UserIsNotAuthorized);
+		await expect(mockContainerRepository.find(containerId)).resolves.toBe(
+			container,
+		);
 	});
 });
