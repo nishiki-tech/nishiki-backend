abeea1a03d0dda4048100123ab78656ddf4d0f61
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-20 17:16:21 -0800

    114 add router and hono adapter for create container controller (#170)
    * fix: remove console.log
    
    * feat: add containers post endpoint
    
    * feat: add contaienrRouter

--- backend/main/src/handler.ts
+++ backend/main/src/handler.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
 import { userRouter } from "src/User/Router/UserRouter";
+import { containerRouter } from "./Group/Router/ContainerRouter";
 
 const app = new Hono();
 
 userRouter(app);
+containerRouter(app);
 
 export const handler = handle(app);
