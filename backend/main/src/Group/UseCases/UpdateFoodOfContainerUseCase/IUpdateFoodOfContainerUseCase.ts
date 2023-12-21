25858a536cb489c0654d9d990fe0b49542d1a45d
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-20 23:22:56 -0800

    feat: add update Food usecase

---
+++ backend/main/src/Group/UseCases/UpdateFoodOfContainerUseCase/IUpdateFoodOfContainerUseCase.ts
@@ -0,0 +1,28 @@
+import { UseCaseError } from "src/Shared";
+import {
+	ContainerIdDomainError,
+	ContainerDomainError,
+} from "src/Group/Domain/Entities/Container";
+
+export interface IUpdateFoodOfContainerUseCase {
+	userId: string;
+	containerId: string;
+	foodId: string;
+	name: string;
+	unit?: string;
+	quantity?: number;
+	expiry?: Date;
+	// TODO: add category to Food entity
+	// category: string;
+}
+
+export class ContainerIsNotExisting extends UseCaseError {}
+export class GroupIsNotExisting extends UseCaseError {}
+export class UserIsNotAuthorized extends UseCaseError {}
+
+export type UpdateFoodOfContainerUseCaseErrorType =
+	| ContainerIsNotExisting
+	| ContainerIdDomainError
+	| ContainerDomainError
+	| GroupIsNotExisting
+	| UserIsNotAuthorized;
