a5b9eb7f115fbd2355c7eaa8fd892ba3bfc02f66
hirohata <hirohatatro@gmail.com>
2023-12-21 11:14:22 +0900

    feat: added an add container test

---
+++ backend/main/test/Shared/Adapters/TestData/Container.ts
@@ -0,0 +1,17 @@
+export const CONTAINER_ID1 = "5270d18f-5cbe-4ca8-b491-eaa65e1d6925";
+export const CONTAINER_ID2 = "9e24cbf1-eab0-40f1-bef2-687751740a03";
+
+export const containerData = {
+    containerData: [
+        {
+            id: CONTAINER_ID1,
+            containerName: "container-1",
+            food: []
+        },
+        {
+            id: CONTAINER_ID2,
+            containerName: "container-2",
+            food: []
+        }
+    ]
+}
