3d2d38c9b7a9dea27b1a8c1b11c192da333a82c9
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-28 15:14:11 -0800

    fix: add handler type

--- backend/main/src/Services/RegisterUserService.ts
+++ backend/main/src/Services/RegisterUserService.ts
@@ -1,25 +1,20 @@
 import { dynamoClient } from "src/Shared/Adapters/DB/DynamoClient";
 import { NishikiDynamoDBClient } from "../Shared/Adapters/DB/NishikiTableClient";
 import { UserId } from "src/User";
-import { CreateUserUseCase } from "src/User/UseCases/CreateUserUseCase/CreateUserUseCase";
-import { UserRepository } from "src/User/Repositories/UserRepository";
-import { CreateUserController } from "src/User/Controllers/CreateUserController";
-import { CreateContainerUseCase } from "src/Group/UseCases/CreateContainerUseCase/CreateContainerUseCase";
-import { CreateContainerController } from "src/Group/Controllers/CreateContainerController";
-import { CreateGroupController } from "src/Group/Controllers/CreateGroupController";
-import { CreateGroupUseCase } from "src/Group/UseCases/CreateGroupUseCase/CreateGroupUseCase";
-import { IGroupDto } from "src/Group/Dtos/GroupDto";
 import { Group, GroupId } from "src/Group/Domain/Entities/Group";
 import { Container, ContainerId } from "src/Group/Domain/Entities/Container";
+import { Callback, Context, PreTokenGenerationTriggerEvent } from "aws-lambda";
 
 const nishikiClient = new NishikiDynamoDBClient(
 	dynamoClient,
 	process.env.TABLE_NAME,
 );
-export const handler = async (event, context, callback) => {
+export const handler = async (
+	event: PreTokenGenerationTriggerEvent,
+	context: Context,
+	callback: Callback,
+) => {
 	console.log("event", event);
-	console.log("context", context);
-	console.log("callback", callback);
 	const userId = UserId.generate();
 	const name = event.userName;
 	const emailAddress = event.request.userAttributes.email;
@@ -48,70 +43,5 @@ export const handler = async (event, context, callback) => {
 		containerIds: [container.id.id],
 	});
 
-	// const userRepository = new UserRepository();
-	// const useCase = new CreateUserUseCase(userRepository);
-	// const controller = new CreateUserController(useCase);
-	// const userSaveResult = await controller.execute({
-	// 	name,
-	// 	emailAddress,
-	// });
-	// const groupIdOrNull = await createInitGroup(userId);
-	// if (!groupIdOrNull) {
-	// 	return callback(null, event);
-	// }
-	// const groupId = groupIdOrNull;
-	// createInitContainer(userId, groupId);
-
-	// const result = await nishikiClient.saveUser({
-	// 	userId: userId.id,
-	// 	username: event.userName,
-	// 	emailAddress: event.request.userAttributes.email,
-	// });
-
 	callback(null, event);
 };
-
-// const createInitGroup = async (userId: UserId): Promise<GroupId | null> => {
-// 	const createGroupUseCase = new CreateGroupUseCase(groupRepository);
-// 	const createGroupController = new CreateGroupController(createGroupUseCase);
-// 	const createGroupResult = await createGroupController.execute({
-// 		userId: userId.id,
-// 	});
-// 	if (!createGroupResult.body) {
-// 		return null;
-// 	}
-// 	if (typeof createGroupResult.body === "string") {
-// 		return null;
-// 	}
-// 	const isGroupDto = (value: any): value is IGroupDto => {
-// 		if (value.id) {
-// 			return true;
-// 		}
-// 		return false;
-// 	};
-// 	if (!isGroupDto(createGroupResult.body)) {
-// 		return null;
-// 	}
-
-// 	const groupIdOrError = GroupId.create(createGroupResult.body?.id);
-// 	if (!groupIdOrError.ok) {
-// 		return null;
-// 	}
-// 	const groupId = groupIdOrError.value;
-
-// 	return groupId;
-// };
-
-// const createInitContainer = async (userId: UserId, groupId: GroupId) => {
-// 	const createContainerUseCase = new CreateContainerUseCase(
-// 		containerRepository,
-// 		groupRepository,
-// 	);
-// 	const createContainerController = new CreateContainerController(
-// 		createContainerUseCase,
-// 	);
-// 	const result = await createContainerController.execute({
-// 		userId: userId.id,
-// 		groupId: groupId.id,
-// 	});
-// };
