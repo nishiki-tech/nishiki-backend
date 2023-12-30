22101ff66f03571ab9ab3404bd81b22597e8a0cd
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2023-12-29 16:23:59 -0800

    fix: addFood usecase

--- backend/main/src/Group/UseCases/AddFoodToContainerUseCase/AddFoodToContainerUseCase.ts
+++ backend/main/src/Group/UseCases/AddFoodToContainerUseCase/AddFoodToContainerUseCase.ts
@@ -40,9 +40,7 @@ export class AddFoodToContainerUseCase
 	public async execute(
 		request: IAddFoodToContainerUseCase,
 	): Promise<Result<undefined, AddFoodToContainerUseCaseErrorType>> {
-		const foodId = FoodId.generate();
-
-		const foodProps: IFoodProps = {
+		const foodProps: Omit<IFoodProps, "createdAt"> = {
 			name: request.name,
 			category: request.category,
 		};
@@ -69,7 +67,7 @@ export class AddFoodToContainerUseCase
 			foodProps.expiry = expiryOrError.value;
 		}
 
-		const foodOrError = Food.create(foodId, foodProps);
+		const foodOrError = Food.generateFoodWithCreatedAt(foodProps);
 		if (!foodOrError.ok) {
 			return Err(foodOrError.error);
 		}
