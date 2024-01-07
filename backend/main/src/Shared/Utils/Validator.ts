9412bf47e95c99f4b37ea0d2f06e4fb240bdd1e6
hirohata <hirohatatro@gmail.com>
2024-01-07 12:33:42 -0800

    feat: added the MD5 validator

--- backend/main/src/Shared/Utils/Validator.ts
+++ backend/main/src/Shared/Utils/Validator.ts
@@ -9,3 +9,12 @@ import { validate, version } from "uuid";
 export const isValidUUIDV4 = (uuid: string): boolean => {
 	return validate(uuid) && version(uuid) === 4;
 };
+
+/**
+ * Check if the given string is MD5.
+ * @param md5String - MD5 string
+ */
+export const isMd5 = (md5String: string): boolean => {
+	const md5Regex = /^[a-f0-9]{32}$/i;
+	return md5Regex.test(md5String);
+}
