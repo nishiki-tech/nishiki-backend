183c0787aeac25207aafc374aadc9c8c7fe89982
hirohata <43802663+hitohata@users.noreply.github.com>
2024-01-07 19:03:59 -0800

    Added the get user endpoint (#219)
    * feat: added find user query
    
    * feat: added new adapters
    
    * feat: connected the query to the controller
    
    * refactor: removed the find user use case.
    
    * fix: wrong comment
    
    * refactor: removed finding multiple user ID patterns
    
    * style: format

--- backend/main/src/User/Router/UserRouter.ts
+++ backend/main/src/User/Router/UserRouter.ts
@@ -1,14 +1,20 @@
 import { Hono } from "hono";
 import {
+	honoInternalServerErrorAdapter,
 	honoNotImplementedAdapter,
+	honoOkResponseAdapter,
 	honoResponseAdapter,
 } from "src/Shared/Adapters/HonoAdapter";
 import { UserRepository } from "src/User/Repositories/UserRepository";
 import { UpdateUserNameUseCase } from "src/User/UseCases/UpdateUserUseCase/UpdateUserNameUseCase";
 import { UpdateUserNameController } from "src/User/Controllers/UpdateUserNameUseCaseController";
 import { getUserService } from "src/Services/GetUserIdService/GetUserService";
+import { FindUserQuery } from "src/User/Query/FindUser/FindUserQuery";
+import { NishikiDynamoDBClient } from "src/Shared/Adapters/DB/NishikiTableClient";
+import { FindUserController } from "src/User/Controllers/FindUserController";
 
 const userRepository = new UserRepository();
+const nishikiDynamoDBClient = new NishikiDynamoDBClient();
 
 /**
  * This is a User router.
@@ -19,9 +25,17 @@ export const userRouter = (app: Hono) => {
 	app.post("/users", async (c) => {
 		return honoNotImplementedAdapter(c);
 	});
+
+	// get user by id.
 	app.get("/users/:id", async (c) => {
-		return honoNotImplementedAdapter(c);
+		const id = c.req.param("id");
+		const query = new FindUserQuery(nishikiDynamoDBClient);
+		const controller = new FindUserController(query);
+		const result = await controller.execute(id);
+		return honoOkResponseAdapter(c, result);
 	});
+
+	// update user name.
 	app.put("/users/:id", async (c) => {
 		// TODO: must be updated after the implementation of this service.
 		const requestingUserId = await getUserService.getUserId("credential"); // get form credential (header)
