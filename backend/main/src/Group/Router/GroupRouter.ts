846a0ba65af17951d7d69dbbd96b8120e993796b
hirohata <hirohatatro@gmail.com>
2024-01-09 09:51:04 -0800

    style: format

--- backend/main/src/Group/Router/GroupRouter.ts
+++ backend/main/src/Group/Router/GroupRouter.ts
@@ -2,15 +2,12 @@ import { Hono } from "hono";
 import {
 	honoMethodNotFoundAdapter,
 	honoNotImplementedAdapter,
-	honoResponseAdapter
+	honoResponseAdapter,
 } from "src/Shared/Adapters/HonoAdapter";
-import {NishikiDynamoDBClient} from "src/Shared/Adapters/DB/NishikiTableClient";
-import {GroupRepository} from "src/Group/Repositories/GroupRepository";
-import {
-	GenerateInvitationLinkHash,
-	InvitationHashService
-} from "src/Services/InvitationHashService/InvitationHashService";
-import {getUserService} from "src/Services/GetUserIdService/GetUserService";
+import { NishikiDynamoDBClient } from "src/Shared/Adapters/DB/NishikiTableClient";
+import { GroupRepository } from "src/Group/Repositories/GroupRepository";
+import { GenerateInvitationLinkHash } from "src/Services/InvitationHashService/InvitationHashService";
+import { getUserService } from "src/Services/GetUserIdService/GetUserService";
 
 const nishikiDynamoDBClient = new NishikiDynamoDBClient();
 const groupRepository = new GroupRepository(nishikiDynamoDBClient);
@@ -41,15 +38,19 @@ export const groupRouter = (app: Hono) => {
 
 		// when generate a new invitation link hash for the group.
 		if (Action === "generateInvitationLink") {
-			const service = new GenerateInvitationLinkHash(nishikiDynamoDBClient, groupRepository);
+			const service = new GenerateInvitationLinkHash(
+				nishikiDynamoDBClient,
+				groupRepository,
+			);
 			const result = await service.execute({
-				groupId, userId
-			})
+				groupId,
+				userId,
+			});
 			return honoResponseAdapter(c, result);
 		}
 
 		return honoMethodNotFoundAdapter(c);
-	})
+	});
 
 	app.get("/groups/:groupId/containers", async (c) => {
 		return honoNotImplementedAdapter(c);
