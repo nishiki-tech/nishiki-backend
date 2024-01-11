22311ba624f9c3cc48f511b407801669eddb48b4
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2024-01-11 00:10:15 -0800

    update: test lambda handler

--- backend/main/src/Services/RegisterUserService.ts
+++ backend/main/src/Services/RegisterUserService.ts
@@ -1,47 +1,54 @@
 import { dynamoClient } from "src/Shared/Adapters/DB/DynamoClient";
 import { NishikiDynamoDBClient } from "../Shared/Adapters/DB/NishikiTableClient";
-import { UserId } from "src/User";
-import { Group, GroupId } from "src/Group/Domain/Entities/Group";
-import { Container, ContainerId } from "src/Group/Domain/Entities/Container";
 import { Callback, Context, PreTokenGenerationTriggerEvent } from "aws-lambda";
+import { CreateANewUserService } from "./CreateNewUserService/CreateANewUserService";
+import { CreateUserUseCase } from "src/User/UseCases/CreateUserUseCase/CreateUserUseCase";
+import { CreateGroupUseCase } from "src/Group/UseCases/CreateGroupUseCase/CreateGroupUseCase";
+import { CreateContainerUseCase } from "src/Group/UseCases/CreateContainerUseCase/CreateContainerUseCase";
+import { GroupRepository } from "src/Group/Repositories/GroupRepository";
+import { UserRepository } from "src/User/Repositories/UserRepository";
+import { ContainerRepository } from "src/Group/Repositories/ContainerRepository";
 
 const nishikiClient = new NishikiDynamoDBClient(
 	dynamoClient,
 	process.env.TABLE_NAME,
 );
+const groupRepository = new GroupRepository();
+const containerRepository = new ContainerRepository();
+const userRepository = new UserRepository();
 export const handler = async (
 	event: PreTokenGenerationTriggerEvent,
 	context: Context,
 	callback: Callback,
 ) => {
 	console.log("event", event);
-	const userId = UserId.generate();
-	const name = event.userName;
+	const name = event.request.userAttributes.nickname;
 	const emailAddress = event.request.userAttributes.email;
-	const result = await nishikiClient.saveUser({
-		userId: userId.id,
-		username: name,
+	const createUser = new CreateUserUseCase(userRepository);
+	const createGroup = new CreateGroupUseCase(groupRepository);
+	const createContainer = new CreateContainerUseCase(
+		containerRepository,
+		groupRepository,
+	);
+	const controller = new CreateANewUserService(
+		createUser,
+		createGroup,
+		createContainer,
+		groupRepository,
+		nishikiClient,
+	);
+
+	const result = await controller.execute({
 		emailAddress: emailAddress,
+		name: name,
 	});
 
-	const containerId = ContainerId.generate();
-	const containerOrError = Container.default(containerId);
-	if (!containerOrError.ok) {
-		return containerOrError.err;
+	if (result.statusCode !== 201) {
+		// throw new Error(
+		// 	`Failed to create a new user. statusCode:${result.statusCode}, body:${result?.body}`,
+		// );
+		callback(new Error("Failed to create a new user."));
 	}
-	const container = containerOrError.value;
-
-	const groupId = GroupId.generate();
-	const groupOrError = Group.default(groupId);
-	if (!groupOrError.ok) {
-		return groupOrError.err;
-	}
-	const group = groupOrError.value;
-	const groupResult = await nishikiClient.saveGroup(groupId.id, {
-		userIds: [userId.id],
-		groupName: group.name,
-		containerIds: [container.id.id],
-	});
 
 	callback(null, event);
 };
