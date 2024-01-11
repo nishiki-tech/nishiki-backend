7e2b939544f9f77ad2addbd2ef681ac27f40c3ab
hirohata <hirohatatro@gmail.com>
2024-01-11 11:22:23 -0800

    feat: added the controller

---
+++ backend/main/src/Group/Controllers/FindAGroupInforamtionController.ts
@@ -0,0 +1,25 @@
+import { Controller } from "src/Shared";
+import { FindAGroupInformation } from "src/Group/Query/FindAGroupInforamtion/FindAGroupInformation";
+
+export class FindAGroupInformationController extends Controller<
+	{ groupId: string },
+	IFindAGroupData | null
+> {
+	readonly query: FindAGroupInformation;
+	constructor(query: FindAGroupInformation) {
+		super();
+		this.query = query;
+	}
+
+	protected async handler(input: { groupId: string }) {
+		const { groupId } = input;
+
+		// this query has no chance occurring errors.
+		return this.ok((await this.query.execute({ groupId })).unwrap());
+	}
+}
+
+interface IFindAGroupData {
+	groupId: string;
+	groupName: string;
+}
