65d6be5107ad1bb7a0e45f9bc214a39540915f9c
hirohata <hirohatatro@gmail.com>
2024-01-14 13:41:24 -0800

    feat: added the test

---
+++ backend/main/test/Group/Query/FindAGroupInformationQuery.test.ts
@@ -0,0 +1,44 @@
+import {afterEach, beforeEach, describe, expect, it, vi} from "vitest";
+import {NishikiDynamoDBClient} from "src/Shared/Adapters/DB/NishikiTableClient";
+import {FindAGroupInformationQuery} from "src/Group/Query/FindAGroupInforamtion/FindAGroupInformationQuery";
+
+describe("find a group information", () => {
+    let mockNishikiDynamoDBClient: NishikiDynamoDBClient;
+    let query: FindAGroupInformationQuery
+
+    const GROUP_INFORMATION = {
+        groupId: "groupId",
+        groupName: "groupName",
+    }
+
+    const GROUP_ID = "8a8bc459-779c-431f-9564-cf97b6662147";
+
+    beforeEach(() => {
+        mockNishikiDynamoDBClient = new NishikiDynamoDBClient();
+        query = new FindAGroupInformationQuery(mockNishikiDynamoDBClient);
+    })
+    afterEach(() => {
+        vi.clearAllMocks();
+    })
+
+    it("Find a group", async () => {
+        vi.spyOn(mockNishikiDynamoDBClient, "getGroup").mockReturnValueOnce(
+            Promise.resolve(GROUP_INFORMATION)
+        );
+
+        const result = await query.execute({groupId: GROUP_ID});
+
+        expect(result.ok).toBeTruthy();
+        expect(result.unwrap()).toEqual(GROUP_INFORMATION);
+    });
+    it("Not find a group, then returns null", async () => {
+        vi.spyOn(mockNishikiDynamoDBClient, "getGroup").mockReturnValueOnce(
+            Promise.resolve(null)
+        );
+
+        const result = await query.execute({groupId: GROUP_ID});
+
+        expect(result.ok).toBeTruthy();
+        expect(result.unwrap()).toBeNull();
+    });
+});
