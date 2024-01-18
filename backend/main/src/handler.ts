cd94cd85a8cc01972d204d059d2dde8357f3967f
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2024-01-17 22:37:37 -0800

    feat: add logger to Hono app

--- backend/main/src/handler.ts
+++ backend/main/src/handler.ts
@@ -2,8 +2,11 @@ import { Hono } from "hono";
 import { handle } from "hono/aws-lambda";
 import { authRouter, userRouter } from "src/User";
 import { containerRouter, groupRouter } from "src/Group";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+app.use("*", logger());
 
 userRouter(app); // /users
 authRouter(app); // /auth
