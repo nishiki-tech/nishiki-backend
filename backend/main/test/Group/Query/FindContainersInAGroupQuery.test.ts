f44b0b2d881f4df7302d618ac2c82547544e3eee
hirohata <hirohatatro@gmail.com>
2024-01-19 13:48:48 -0800

    style: format

--- backend/main/test/Group/Query/FindContainersInAGroupQuery.test.ts
+++ backend/main/test/Group/Query/FindContainersInAGroupQuery.test.ts
@@ -1,45 +1,53 @@
-import { vi, describe, it, beforeEach, expect } from "vitest"
-import { NishikiDynamoDBClient } from "src/Shared/Adapters/DB/NishikiTableClient"
-import {FindContainersInAGroupQuery, InvalidUUId, GroupNotFound} from "src/Group/Query/FindContainersInAGroupQuery/FindContainersInAGroupQuery";
+import { vi, describe, it, beforeEach, expect } from "vitest";
+import { NishikiDynamoDBClient } from "src/Shared/Adapters/DB/NishikiTableClient";
+import {
+	FindContainersInAGroupQuery,
+	InvalidUUId,
+	GroupNotFound,
+} from "src/Group/Query/FindContainersInAGroupQuery/FindContainersInAGroupQuery";
 
 describe("Find containers in a group", () => {
-    let nishikiDynamoDb: NishikiDynamoDBClient;
-    let query: FindContainersInAGroupQuery;
+	let nishikiDynamoDb: NishikiDynamoDBClient;
+	let query: FindContainersInAGroupQuery;
 
-    const GROUP_ID = "e7e5b821-2e96-4042-81b5-ae62cf9a7559";
+	const GROUP_ID = "e7e5b821-2e96-4042-81b5-ae62cf9a7559";
 
-    beforeEach(() => {
-        nishikiDynamoDb = new NishikiDynamoDBClient();
-        query = new FindContainersInAGroupQuery(nishikiDynamoDb);
-    })
+	beforeEach(() => {
+		nishikiDynamoDb = new NishikiDynamoDBClient();
+		query = new FindContainersInAGroupQuery(nishikiDynamoDb);
+	});
 
-    it("invalid id", async () => {
-        const result = await query.execute({groupId: "invalid_id"});
-        expect(result.err).toBeTruthy();
-        expect(result.unwrapError()).toBeInstanceOf(InvalidUUId);
-    })
+	it("invalid id", async () => {
+		const result = await query.execute({ groupId: "invalid_id" });
+		expect(result.err).toBeTruthy();
+		expect(result.unwrapError()).toBeInstanceOf(InvalidUUId);
+	});
 
-    it("a group is not found", async () => {
-        vi.spyOn(nishikiDynamoDb, "listOfContainers").mockReturnValueOnce(
-            Promise.resolve([])
-        );
-        vi.spyOn(nishikiDynamoDb, "getGroup").mockReturnValueOnce(Promise.resolve(null)
-        );
-        const result = await query.execute({groupId: GROUP_ID});
-        expect(result.err).toBeTruthy();
-        expect(result.unwrapError()).toBeInstanceOf(GroupNotFound);
-    })
+	it("a group is not found", async () => {
+		vi.spyOn(nishikiDynamoDb, "listOfContainers").mockReturnValueOnce(
+			Promise.resolve([]),
+		);
+		vi.spyOn(nishikiDynamoDb, "getGroup").mockReturnValueOnce(
+			Promise.resolve(null),
+		);
+		const result = await query.execute({ groupId: GROUP_ID });
+		expect(result.err).toBeTruthy();
+		expect(result.unwrapError()).toBeInstanceOf(GroupNotFound);
+	});
 
-    it("no containers information has found", async () => {
-        vi.spyOn(nishikiDynamoDb, "listOfContainers").mockReturnValueOnce(
-            Promise.resolve(["container-id"])
-        );
-        vi.spyOn(nishikiDynamoDb, "getGroup").mockReturnValueOnce(Promise.resolve({groupId: "", groupName: ""})
-        );
-        vi.spyOn(nishikiDynamoDb, "getContainer").mockReturnValueOnce(Promise.resolve(null));
-        const result = await query.execute({groupId: GROUP_ID});
-        console.log(result)
-        expect(result.ok).toBeTruthy();
-        expect(result.unwrap()).toEqual({containers: []});
-    })
-})
+	it("no containers information has found", async () => {
+		vi.spyOn(nishikiDynamoDb, "listOfContainers").mockReturnValueOnce(
+			Promise.resolve(["container-id"]),
+		);
+		vi.spyOn(nishikiDynamoDb, "getGroup").mockReturnValueOnce(
+			Promise.resolve({ groupId: "", groupName: "" }),
+		);
+		vi.spyOn(nishikiDynamoDb, "getContainer").mockReturnValueOnce(
+			Promise.resolve(null),
+		);
+		const result = await query.execute({ groupId: GROUP_ID });
+		console.log(result);
+		expect(result.ok).toBeTruthy();
+		expect(result.unwrap()).toEqual({ containers: [] });
+	});
+});
