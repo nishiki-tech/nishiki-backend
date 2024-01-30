65b1f3ab470a95f0f87dd971602c876a261c64e0
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2024-01-30 12:48:24 -0800

    feat: add UserIsNotAuthorized error

--- backend/main/src/Group/UseCases/CreateGroupUseCase/ICreateGroupUseCase.ts
+++ backend/main/src/Group/UseCases/CreateGroupUseCase/ICreateGroupUseCase.ts
@@ -1,8 +1,12 @@
 import { GroupDomainError } from "src/Group/Domain/Entities/Group";
+import { UseCaseError } from "src/Shared";
 
 export interface ICreateGroupUseCase {
 	name?: string;
 	userId: string;
 }
+export class UserIsNotAuthorized extends UseCaseError {}
 
-export type CreateGroupUseCaseErrorType = GroupDomainError;
+export type CreateGroupUseCaseErrorType =
+	| GroupDomainError
+	| UserIsNotAuthorized;
