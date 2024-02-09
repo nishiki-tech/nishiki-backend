4741934e9600ef40cdb5210aa9aa1c120560f745
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2024-02-09 10:17:57 -0800

    refactor: remove for loop to stop blocking process

--- backend/main/src/Group/Query/FindContainersOfAUserQuery/FindContainersOfAUserQuery.ts
+++ backend/main/src/Group/Query/FindContainersOfAUserQuery/FindContainersOfAUserQuery.ts
@@ -1,6 +1,9 @@
 import { IQuery } from "src/Shared/Layers/Query/IQuery";
 import { NishikiDynamoDBClient } from "src/Shared/Adapters/DB/NishikiTableClient";
-import { ContainerData } from "src/Shared/Adapters/DB/NishikiDBTypes";
+import {
+	ContainerData,
+	UsersGroup,
+} from "src/Shared/Adapters/DB/NishikiDBTypes";
 import { Ok, Err, Result } from "result-ts-type";
 import { QueryError } from "src/Shared/Utils/Errors";
 import { isValidUUIDV4 } from "src/Shared/Utils/Validator";
@@ -21,51 +24,67 @@ export class FindContainersOfAUserQuery
 		const userGroups =
 			await this.nishikiDynamoDBClient.listOfUsersGroup(userId);
 
-		const containersOfUser: IContainerData = { containers: [] };
-
-		for (const userGroup of userGroups) {
-			const [containerIds, groupData] = await Promise.all([
-				this.nishikiDynamoDBClient.listOfContainers(userGroup.groupId),
-				this.nishikiDynamoDBClient.getGroup({ groupId: userGroup.groupId }),
-			]);
-			if (!groupData) {
-				continue;
-			}
-			if (containerIds.length === 0) {
-				continue;
-			}
-
-			const containersResult = await Promise.all(
-				containerIds.map((containerId) =>
-					this.nishikiDynamoDBClient.getContainer(containerId),
-				),
-			);
-
-			const containers = containersResult.filter(
-				(container) => container,
-			) as ContainerData[];
-
-			for (const container of containers) {
-				containersOfUser.containers.push({
-					id: container.containerId,
-					name: container.containerName,
-					group: {
-						userId: userId,
-						groupName: groupData.groupName,
-					},
-					foods: container.foods.map((food) => ({
-						id: food.foodId,
-						name: food.name,
-						unit: food.unit,
-						quantity: food.quantity,
-						category: food.category,
-						expiry: food.expiry,
-						createdAt: food.createdAt,
-					})),
-				});
-			}
+		if (userGroups.length === 0) {
+			return Ok({ containers: [] });
 		}
-		return Ok(containersOfUser);
+
+		const groupContainerDetailsList = (
+			await Promise.all(
+				userGroups.map(async (userGroup) => {
+					return [...(await this.getGroupContainerDetails(userGroup))];
+				}),
+			)
+		).flat();
+
+		return Ok({ containers: groupContainerDetailsList });
+	}
+
+	private async getGroupContainerDetails(
+		userGroup: UsersGroup,
+	): Promise<IContainer[]> {
+		const [groupData, containerIds] = await Promise.all([
+			this.nishikiDynamoDBClient.getGroup({
+				groupId: userGroup.groupId,
+			}),
+			this.nishikiDynamoDBClient.listOfContainers(userGroup.groupId),
+		]);
+
+		if (!groupData) {
+			return [];
+		}
+		if (containerIds.length === 0) {
+			return [];
+		}
+
+		const containersResult = await Promise.all(
+			containerIds.map((containerId) =>
+				this.nishikiDynamoDBClient.getContainer(containerId),
+			),
+		);
+
+		const containers = containersResult.filter(
+			(container) => container !== null,
+		) as ContainerData[];
+
+		return containers.map((container) => {
+			return {
+				id: container.containerId,
+				name: container.containerName,
+				group: {
+					groupId: groupData.groupId,
+					groupName: groupData.groupName,
+				},
+				foods: container.foods.map((food) => ({
+					id: food.foodId,
+					name: food.name,
+					unit: food.unit,
+					quantity: food.quantity,
+					category: food.category,
+					expiry: food.expiry,
+					createdAt: food.createdAt,
+				})),
+			};
+		});
 	}
 }
 
@@ -79,7 +98,7 @@ interface IContainer {
 	id: string;
 	name: string;
 	group: {
-		userId: string;
+		groupId: string;
 		groupName: string;
 	};
 	foods: {
