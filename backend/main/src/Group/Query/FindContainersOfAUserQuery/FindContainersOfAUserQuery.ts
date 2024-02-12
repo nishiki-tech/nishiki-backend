fc5c7f97996e22108e8a08c91f1f3c4d7e73ab95
Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
2024-02-11 19:50:09 -0800

    Feature/173 implement get containers (#289)
    * feat: add find containers of a user controller
    
    * feat: add find containers of a user query
    
    * feat: implement GET containers
    
    * refactor: remove for loop to stop blocking process
    
    * fix: unnecessary spreading
    
    * fix: Create container usecase update group repository.
    
    * fix: the condition was wrong
    
    * Revert "fix: unnecessary spreading"
    
    This reverts commit b127dd144658387feaec57634d371d6c7d9f90d7.
    
    * fix: unnecessary spreading
    
    * Revert "fix: Create container usecase update group repository."
    
    This reverts commit 149186e910b3532b38221194c70f0029b6e790f8.
    
    * refactor: combine two if statement
    
    * Update backend/main/src/Group/Query/FindContainersOfAUserQuery/FindContainersOfAUserQuery.ts
    
    Co-authored-by: hirohata <43802663+hitohata@users.noreply.github.com>
    
    * chore: format
    
    ---------
    
    Co-authored-by: hirohata <43802663+hitohata@users.noreply.github.com>

---
+++ backend/main/src/Group/Query/FindContainersOfAUserQuery/FindContainersOfAUserQuery.ts
@@ -0,0 +1,108 @@
+import { IQuery } from "src/Shared/Layers/Query/IQuery";
+import { NishikiDynamoDBClient } from "src/Shared/Adapters/DB/NishikiTableClient";
+import {
+	ContainerData,
+	UsersGroup,
+} from "src/Shared/Adapters/DB/NishikiDBTypes";
+import { Ok, Err, Result } from "result-ts-type";
+import { QueryError } from "src/Shared/Utils/Errors";
+import { isValidUUIDV4 } from "src/Shared/Utils/Validator";
+
+export class FindContainersOfAUserQuery
+	implements IQuery<{ userId: string }, IContainerData, InvalidUUId>
+{
+	constructor(private readonly nishikiDynamoDBClient: NishikiDynamoDBClient) {}
+
+	public async execute(input: { userId: string }): Promise<
+		Result<IContainerData, InvalidUUId>
+	> {
+		const { userId } = input;
+		if (!isValidUUIDV4(userId)) {
+			return Err(new InvalidUUId("User ID is invalid"));
+		}
+
+		const userGroups =
+			await this.nishikiDynamoDBClient.listOfUsersGroup(userId);
+
+		if (userGroups.length === 0) {
+			return Ok({ containers: [] });
+		}
+
+		const groupContainerDetailsList = (
+			await Promise.all(
+				userGroups.map((userGroup) => this.getGroupContainerDetails(userGroup)),
+			)
+		).flat();
+
+		return Ok({ containers: groupContainerDetailsList });
+	}
+
+	private async getGroupContainerDetails(
+		userGroup: UsersGroup,
+	): Promise<IContainer[]> {
+		const [groupData, containerIds] = await Promise.all([
+			this.nishikiDynamoDBClient.getGroup({
+				groupId: userGroup.groupId,
+			}),
+			this.nishikiDynamoDBClient.listOfContainers(userGroup.groupId),
+		]);
+
+		if (!(groupData && containerIds.length > 0)) {
+			return [];
+		}
+
+		const containersResult = await Promise.all(
+			containerIds.map((containerId) =>
+				this.nishikiDynamoDBClient.getContainer(containerId),
+			),
+		);
+
+		const containers = containersResult.filter(
+			(container) => container !== null,
+		) as ContainerData[];
+
+		return containers.map((container) => {
+			return {
+				id: container.containerId,
+				name: container.containerName,
+				group: {
+					groupId: groupData.groupId,
+					groupName: groupData.groupName,
+				},
+				foods: container.foods.map((food) => ({
+					id: food.foodId,
+					name: food.name,
+					unit: food.unit,
+					quantity: food.quantity,
+					category: food.category,
+					expiry: food.expiry,
+					createdAt: food.createdAt,
+				})),
+			};
+		});
+	}
+}
+
+export class InvalidUUId extends QueryError {}
+
+export interface IContainerData {
+	containers: IContainer[];
+}
+
+interface IContainer {
+	id: string;
+	name: string;
+	group: {
+		groupId: string;
+		groupName: string;
+	};
+	foods: {
+		id: string;
+		name: string;
+		quantity: number | null;
+		category: string;
+		unit: string | null;
+		expiry: string | null;
+		createdAt: string;
+	}[];
+}
