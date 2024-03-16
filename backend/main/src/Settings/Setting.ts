393ca4f06187677268db8de88ebbaa32e9536bf6
hirohata <43802663+hitohata@users.noreply.github.com>
2024-03-15 19:04:36 -0700

    Added delete user endpoint (#318)
    * feat: added a new API
    
    * feat: added a new environment value
    
    * feat: added a delete user service
    
    * feat: added an endpoint
    
    * feat: updated the main function role
    
    * chore: format
    
    * Update backend/main/src/Services/DeleteUserService/DeleteUserService.ts
    
    Co-authored-by: Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
    
    * Update backend/main/src/Services/DeleteUserService/DeleteUserService.ts
    
    Co-authored-by: Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>
    
    * fix: updated an input value of the controller
    
    * fix: updated a variable name
    
    * fix: comment
    
    ---------
    
    Co-authored-by: Taniguchi <70570221+taniguchiiqqq@users.noreply.github.com>

--- backend/main/src/Settings/Setting.ts
+++ backend/main/src/Settings/Setting.ts
@@ -3,3 +3,5 @@ export const TABLE_NAME: string = process.env.TABLE_NAME!;
 
 // Region of the DynamoDB.
 export const REGION: string = process.env.REGION!;
+
+export const COGNITO_USER_POOL_ID = process.env.USER_POOL_ID!;
--- cdk/lib/nishikiBackend-stack.ts
+++ cdk/lib/nishikiBackend-stack.ts
@@ -24,6 +24,7 @@ export class NishikiBackendStack extends cdk.Stack {
 
 		const mainFunction = nishikiMainBackendFunction(this, stage, {
 			tableName: table.tableName,
+			userPool,
 		});
 
 		// add permission to writing and reading of Dynamo DB.
@@ -145,6 +146,7 @@ const nishikiBackendAPIAuthorizer = (scope: Construct, userPool: UserPool) => {
 
 interface IMainFunctionProps {
 	tableName: string;
+	userPool: UserPool;
 }
 
 /**
@@ -158,7 +160,7 @@ const nishikiMainBackendFunction = (
 	stage: Stage,
 	props: IMainFunctionProps,
 ): NodejsFunction => {
-	const { tableName } = props;
+	const { tableName, userPool } = props;
 
 	const mainFunction = new lambdaNode.NodejsFunction(
 		scope,
@@ -175,8 +177,17 @@ const nishikiMainBackendFunction = (
 			),
 			environment: {
 				TABLE_NAME: tableName,
+				USER_POOL_ID: userPool.userPoolId,
 			},
 		},
+	);
+
+	// add a permission to remove user data form user pool.
+	mainFunction.addToRolePolicy(
+		new iam.PolicyStatement({
+			actions: ["cognito-idp:ListUsers", "cognito-idp:AdminDeleteUser"],
+			resources: [userPool.userPoolArn],
+		}),
 	);
 
 	return mainFunction;
